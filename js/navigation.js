343cb27b169b5b611c58b49b27350105e5bd73b8
Jason Booth <hello@studiojasonbooth.com>
2019-03-06 15:55:26 +0100

    added img assets and added video background to hero section

--- js/navigation.js
+++ js/navigation.js
@@ -3,17 +3,6 @@ const navToggler = document.querySelector('button.navbar-toggler')
 
 const menuDropdown = document.querySelector('div.collapse')
 
-
 navToggler.addEventListener('click', function(){
     menuDropdown.classList.toggle('active')
 })
-
-
-
-
-
-
-
-// $('.navbar-toggler').on('click',function() {
-//     alert("Hello! I am an alert box!!");
-//   });
