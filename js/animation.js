d52484089e54fcc3c8245fde7684083235326e85
Jason Booth <hello@studiojasonbooth.com>
2019-03-07 15:43:48 +0100

    changed animation js file

--- js/animation.js
+++ js/animation.js
@@ -4,20 +4,20 @@ const animatedTags = document.querySelectorAll("h1, h2, h3, h4, p, .fadein, a.bt
 
 animatedTags.forEach(tag =>{
     tag.style.opacity = 0
-    tag.style.transform = 'translateY(40px)'
+    tag.style.transform = 'translateY(36px)'
   })
   
   const fadeIn = function() {
     
-    let delay = 0.08
+    let delay = 0.07
     
     animatedTags.forEach(tag =>{
       const tagTop = tag.getBoundingClientRect().top
       const tagBottom = tag.getBoundingClientRect().bottom
          
       if(tagTop < window.innerHeight && tagBottom > 0) {
-        tag.style.animation = `fadein 0.8s ${delay}s both`
-        delay = delay + 0.08
+        tag.style.animation = `fadein 1.1s ${delay}s both`
+        delay = delay + 0.07
       } else {
         tag.style.opacity = 0
         tag.style.animation = ""
