332cdba220c5cb0e831e702fdbbef1942cede7e9
Jason Booth <hello@studiojasonbooth.com>
2019-03-08 13:45:07 +0100

    mobile styling across the whole site

--- js/navigation.js
+++
@@ -1,8 +0,0 @@
-
-const navToggler = document.querySelector('button.navbar-toggler')
-
-const menuDropdown = document.querySelector('div.collapse')
-
-navToggler.addEventListener('click', function(){
-    menuDropdown.classList.toggle('active')
-})
