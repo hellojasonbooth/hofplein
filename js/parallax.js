7f5af3684e98331712b30a044614645d4c7716c1
Jason Booth <hello@studiojasonbooth.com>
2019-03-25 15:54:10 +0100

    bug fix on the homepage video

--- js/parallax.js
+++ js/parallax.js
@@ -4,6 +4,6 @@ $(document).on('scroll', function () {
 
     var pixels = $(document).scrollTop()
 
-    $("div.video-container").css("top", pixels * -0.5)
+    $("div.video-container").css("top", pixels * -0.4)
 
 })
